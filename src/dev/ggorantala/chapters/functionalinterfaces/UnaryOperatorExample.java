934d270c77b19bdd954738d75a5c21e398955d86
Gopi Gorantala <gopishiva001@outlook.com>
2023-06-02 19:47:43 +0530

    file name changes

---
+++ src/dev/ggorantala/chapters/functionalinterfaces/UnaryOperatorExample.java
@@ -0,0 +1,22 @@
+package src.dev.ggorantala.chapters.functionalinterfaces;
+/*
+ * This code is part of the course "Master Java 8(11/17) Functional Programming With Lambdas And Streams API"
+ * for https://ggorantala.dev.
+ *
+ * Copyright (C) 2023 by Gopi Gorantala.
+ *
+ * @author ggorantala
+ * Date: 07/05/23
+ */
+
+import java.util.function.UnaryOperator;
+import src.dev.ggorantala.Constants;
+
+public class UnaryOperatorExample {
+  public static void main(String[] args) {
+    UnaryOperator<Integer> doubleOperator = x -> x * 2;
+    int result = doubleOperator.apply(Constants.VALUE); // result will be 10
+
+    System.out.println(result);
+  }
+}
